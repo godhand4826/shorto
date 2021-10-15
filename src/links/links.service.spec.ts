dd254675378ea949f9c2e3f916181b459abe4a51
eric_huang <godhand4826@gmail.com>
2021-10-15 16:55:40 +0800

    ✅ Add links and users service test

--- src/links/links.service.spec.ts
+++ src/links/links.service.spec.ts
@@ -2,9 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { LinksService } from './links.service';
 import { Link } from './entities/link.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
 
 describe('LinksService', () => {
   let service: LinksService;
+  let repo: Repository<Link>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,15 +15,35 @@ describe('LinksService', () => {
         LinksService,
         {
           provide: getRepositoryToken(Link),
-          useValue: {},
+          useClass: Repository,
         },
       ],
     }).compile();
 
     service = module.get<LinksService>(LinksService);
+    repo = module.get<Repository<Link>>(getRepositoryToken(Link));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('find long URL', async () => {
+    const link = new Link();
+    link.url = 'http://example.com';
+    link.views = 0;
+    jest.spyOn(repo, 'findOne').mockImplementation(async () => link);
+    jest.spyOn(repo, 'save').mockImplementation(async link => link as Link);
+
+    const url = await service.findLongURL('A');
+
+    expect(url).toEqual(link.url);
+    expect(link.views).toEqual(1);
+  });
+
+  it('could not find long URL', async () => {
+    jest.spyOn(repo, 'findOne').mockImplementation(async () => null);
+
+    await expect(service.findLongURL('A')).rejects.toThrow(NotFoundException);
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,26 +1,80 @@
+import { ConflictException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { VerifyUserDto } from './dto/verify-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repo: Repository<User>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
         {
           provide: getRepositoryToken(User),
-          useValue: {},
+          useClass: Repository,
         },
       ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repo = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('register', async () => {
+    jest.spyOn(repo, 'findOne').mockImplementation(async () => null);
+    jest.spyOn(repo, 'create').mockImplementation(newUser => {
+      const user = new User();
+      user.id = 1;
+      user.account = newUser.account;
+      user.password = newUser.password;
+      return user;
+    });
+    jest.spyOn(repo, 'save').mockImplementation(async user => {
+      return user as User;
+    });
+
+    const newUser: CreateUserDto = { account: 'eric', password: 'pass' };
+    const user = await service.register(newUser);
+
+    expect(user.account).toEqual(newUser.account);
+    expect((user as any).password).toBeUndefined();
+    expect(user.token).toBeDefined();
+    expect(user.token.length).not.toBe(0);
+  });
+
+  it('register conflict', async () => {
+    jest.spyOn(repo, 'findOne').mockImplementation(async () => new User());
+
+    const newUser: CreateUserDto = { account: 'eric', password: 'pass' };
+
+    await expect(service.register(newUser)).rejects.toThrow(ConflictException);
+  });
+
+  it('login', async () => {
+    const user = new User();
+    user.account = 'eric';
+    user.password = 'pass';
+    jest.spyOn(repo, 'findOne').mockImplementation(async () => user);
+    jest.spyOn(user, 'comparePassword').mockImplementation(async function (p) {
+      return p == this.password;
+    });
+
+    const claim: VerifyUserDto = { account: 'eric', password: 'pass' };
+    const u = await service.login(claim);
+
+    expect(u.account).toEqual(user.account);
+    expect((u as any).password).toBeUndefined();
+    expect(u.token).toBeDefined();
+    expect(u.token.length).not.toBe(0);
   });
 });
